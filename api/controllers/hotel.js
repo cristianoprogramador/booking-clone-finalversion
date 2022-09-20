984c0810b5f8b077a7a2389a11bd2aa2c85f5e62
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2022-09-19 22:05:35 -0300

    Conexão entre as rotas e paginas criadas e a pagina de pesquisar concluida

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -1,6 +1,6 @@
 import Hotel from "../models/Hotel.js"
 
-export const createHotel = async (req,res,next) => {
+export const createHotel = async (req, res, next) => {
     const newHotel = new Hotel(req.body)
 
     try {
@@ -10,7 +10,7 @@ export const createHotel = async (req,res,next) => {
         next(err)
     }
 }
-export const updateHotel = async (req,res,next) => {
+export const updateHotel = async (req, res, next) => {
     try {
         const updatedHotel = await Hotel.findByIdAndUpdate(
             req.params.id,
@@ -21,7 +21,7 @@ export const updateHotel = async (req,res,next) => {
         next(err)
     }
 }
-export const deleteHotel = async (req,res,next) => {
+export const deleteHotel = async (req, res, next) => {
     try {
         await Hotel.findByIdAndDelete(
             req.params.id
@@ -31,7 +31,7 @@ export const deleteHotel = async (req,res,next) => {
         next(err)
     }
 }
-export const getHotel = async (req,res,next) => {
+export const getHotel = async (req, res, next) => {
     try {
         const hotel = await Hotel.findById(req.params.id);
         res.status(200).json(hotel)
@@ -39,10 +39,45 @@ export const getHotel = async (req,res,next) => {
         next(err)
     }
 }
-export const getHotels = async (req,res,next) => {
+export const getHotels = async (req, res, next) => {
+    const {min, max, ...others} = req.query
     try {
-        const hotels = await Hotel.find();
+        const hotels = await Hotel.find({...others, cheapestPrice: {$gt:min | 1, $lt:max || 9999}})
+        .limit(req.query.limit);
         res.status(200).json(hotels)
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const countByCity = async (req, res, next) => {
+    const cities = req.query.cities.split(',')
+    try {
+        const list = await Promise.all(cities.map(city => {
+            return Hotel.countDocuments({ city: city })
+        }))
+        res.status(200).json(list)
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const countByType = async (req, res, next) => {
+    try {
+        const hotelCount = await Hotel.countDocuments({ type: "hotel" })
+        const apartmentCount = await Hotel.countDocuments({ type: "apartment" })
+        const resortCount = await Hotel.countDocuments({ type: "resort" })
+        const villaCount = await Hotel.countDocuments({ type: "villa" })
+        const cabinCount = await Hotel.countDocuments({ type: "cabin" })
+
+
+        res.status(200).json([
+            { type: "hotel", count: hotelCount },
+            { type: "apartments", count: apartmentCount },
+            { type: "resorts", count: resortCount },
+            { type: "villas", count: villaCount },
+            { type: "cabins", count: cabinCount },
+        ])
     } catch (err) {
         next(err)
     }
